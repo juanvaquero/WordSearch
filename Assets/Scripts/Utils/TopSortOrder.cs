d27fc6749dc6dd70ad6ef84a94c44c310bc51a42
Juan Antonio Arnau <juan_arnau@hotmail.es>
2024-07-26 13:28:21 +0200

    feat: Add another tool script to change the sort render order of UI. And move util scripts to Utils folder.

---
+++ Assets/Scripts/Utils/TopSortOrder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class TopSortOrder : MonoBehaviour {
+
+	void Start () {
+		int mayorSortingOrder = 2;
+		foreach (var gameobject in GameObject.FindObjectsOfType<Canvas>())
+		{
+			int shortOrder = gameobject.GetComponent<Canvas>().sortingOrder;
+			mayorSortingOrder = mayorSortingOrder <= shortOrder ? shortOrder + 1 : mayorSortingOrder;
+		}
+		GetComponent<Canvas>().sortingOrder = mayorSortingOrder;
+	}
+}
